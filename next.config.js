7f308626adc5d63c6ca8fe51402c4598dca3d170
Kat <hi.connkat@gmail.com>
2025-12-24 13:11:31 -0800

    added sanity

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
-  output: 'export',
   compress: true,
   generateEtags: true,
   pageExtensions: ['tsx', 'mdx', 'ts'],
